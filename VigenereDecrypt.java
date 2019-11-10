e77ce282fb27cd5659a41cf75315afa7f8b1ff2a
David W. Arnold <d.w.arnold.gm@gmail.com>
2019-11-10 19:49:13 +0000

    Vigenere Cipher (Exercise 3) frist draft of implementation - taking too long to run on raptor, make exponentially faster

--- VigenereDecrypt.java
+++ VigenereDecrypt.java
@@ -9,15 +9,69 @@ import java.io.IOException;
  */
 public class VigenereDecrypt extends Decrypt
 {
+    private String pt;
+    private boolean plaintextFound;
+
     public VigenereDecrypt(File tessFile, File cipherFile) throws IOException
     {
         super(tessFile, cipherFile);
+        this.pt = "";
+        this.plaintextFound = false;
     }
 
     /**
      * Decrypt with a provided key.
      */
     public String decrypt(String key)
+    {
+        decryptPrivate(key);
+        return pt;
+    }
+
+    /**
+     * Decrypt with an unknown key which is an arbitary sequence of letters.
+     * @param keyLen Length of the unknown key.
+     */
+    public String decrypt(int keyLen)
+    {
+        checkAllKeys(charAlphabet, keyLen);
+        if (plaintextFound) {
+            return pt;
+        } else {
+            return "";
+        }
+    }
+
+    // The method that tries all decryption keys of a given length k.
+    // It is mainly a wrapper over recursive function checkAllKeysRec()
+    private void checkAllKeys(char[] set, int k)
+    {
+        checkAllKeysRec(set, "", set.length, k);
+    }
+
+    // The main recursive method to try decryption with all possible strings of length k.
+    private void checkAllKeysRec(char[] set, String prefix, int n, int k)
+    {
+        // Base case: k is 0, print prefix
+        if (k != 0) {
+            // One by one add all characters from set and recursively call for k equals to k-1
+            for (int i = 0; i < n; ++i) {
+                // Next character of input added
+                String newPrefix = prefix + set[i];
+                // k is decreased, because we have added a new character
+                checkAllKeysRec(set, newPrefix, n, k - 1);
+            }
+        } else {
+            System.out.println(prefix);
+            decryptPrivate(prefix);
+            if (tess.contains(pt)) {
+                // We've found our plaintext.
+                return;
+            }
+        }
+    }
+
+    private void decryptPrivate(String key)
     {
         String decryptedPlaintext = "";
         char[] keyCharacters = key.toCharArray();
@@ -40,18 +94,6 @@ public class VigenereDecrypt extends Decrypt
             System.out.println("Key: " + key);
             System.out.println();
         }
-        return decryptedPlaintext;
-    }
-
-    /**
-     * Decrypt with an unknown key which is an arbitary sequence of letters.
-     *
-     * @param len Length of the unknown key.
-     */
-    public String decrypt(int len)
-    {
-
-
-        return "";
+        pt = decryptedPlaintext;
     }
 }
