150d9f1e64eea557f9c1f71672105b81d7f9e608
David W. Arnold <d.w.arnold.gm@gmail.com>
2019-12-02 13:44:42 +0000

    Caesar cipher now checks for common pairs and repeats of english letters before checking tess

--- CaesarDecrypt.java
+++ CaesarDecrypt.java
@@ -1,5 +1,9 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Decryption of ciphertext encoded with a Caesar Cipher.
@@ -9,12 +13,27 @@ import java.io.IOException;
  */
 public class CaesarDecrypt extends Decrypt
 {
+    final private ArrayList<String> ENGLISH_COMMON_PAIRS_AND_REPEATS = new ArrayList<String>()
+    {{
+        add("TH");
+        add("ER");
+        add("ON");
+        add("AN");
+        add("SS");
+        add("EE");
+        add("TT");
+        add("FF");
+    }};
     private String pt;
+    private HashMap<Character, Integer> occurrences;
+    private HashMap<Character, String> decryptedPlaintexts;
 
     public CaesarDecrypt(File tessFile, File cipherFile) throws IOException
     {
         super(tessFile, cipherFile);
         this.pt = "";
+        this.occurrences = new HashMap<>();
+        this.decryptedPlaintexts = new HashMap<>();
     }
 
     public String decrypt()
@@ -28,14 +47,30 @@ public class CaesarDecrypt extends Decrypt
                 int c = findIndex(charAlphabet, ciphertext.charAt(j));
                 plaintext += charAlphabet[(charAlphaLen + (c - i)) % charAlphaLen];
             }
-            // If the tess??.txt file contains the decrypted plaintext, it must be the correct decryption.
-            if (tess.contains(plaintext)) {
-                System.out.println(getExercise(cipherFile) + ": Caesar Cipher");
-                System.out.println("Decrypted: " + plaintext);
-                System.out.println("Key: " + charAlphabet[i]);
-                System.out.println();
-                pt = plaintext;
+            System.out.println();
+            int totalOccurrences = 0;
+            for (String commonPairOrRepeat : ENGLISH_COMMON_PAIRS_AND_REPEATS) {
+                int lastIndex = 0;
+                while (lastIndex != -1) {
+                    lastIndex = plaintext.indexOf(commonPairOrRepeat, lastIndex);
+                    if (lastIndex != -1) {
+                        totalOccurrences++;
+                        lastIndex += commonPairOrRepeat.length();
+                    }
+                }
             }
+            occurrences.put(charAlphabet[i], totalOccurrences);
+            decryptedPlaintexts.put(charAlphabet[i], plaintext);
+        }
+        char maximumOccurrencesKey = occurrences.entrySet().stream().max(Comparator.comparing(Map.Entry::getValue)).get().getKey();
+        String potentialPlaintext = decryptedPlaintexts.get(maximumOccurrencesKey);
+        // If the tess??.txt file contains the decrypted plaintext, it must be the correct decryption.
+        if (tess.contains(potentialPlaintext)) {
+            System.out.println(getExercise(cipherFile) + ": Caesar Cipher");
+            System.out.println("Decrypted: " + potentialPlaintext);
+            System.out.println("Key: " + maximumOccurrencesKey);
+            System.out.println();
+            pt = potentialPlaintext;
         }
         return pt;
     }
